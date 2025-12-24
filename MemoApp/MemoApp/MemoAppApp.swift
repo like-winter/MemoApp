a8bae0ffcb05738251898ded4552bceefbf8ba34
like-winter <jeolmi78@gmail.com>
2025-12-25 01:13:24 +0900

    chore: 프로젝트 초기 설정

---
+++ MemoApp/MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by Min on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
