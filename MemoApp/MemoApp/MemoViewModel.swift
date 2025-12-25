dd79ccf9ded6bf9fe46825fbf4cc77f944920bca
like-winter <jeolmi78@gmail.com>
2025-12-26 04:23:03 +0900

    feat: add CRUD functions to MemoViewModel

---
+++ MemoApp/MemoApp/MemoViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  MemoViewModel.swift
+//  MemoApp
+//
+//  Created by Min on 12/26/25.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+final class MemoViewModel: ObservableObject {
+    @Published var memos: [Memo] = []
+    
+    func add(title: String, content: String) {
+        let memo = Memo(title: title, content: content, date: Date())
+    }
+    
+    func delete(at offsets: IndexSet) {
+        memos.remove(atOffsets: offsets)
+    }
+    
+    func update(id: UUID, title: String, content: String) -> Bool {
+        guard let index = memos.firstIndex(where: { $0.id == id}) else {
+            return false
+        }
+        
+        memos[index].title = title
+        memos[index].content = content
+        memos[index].date = Date()
+        
+        return true
+    }
+}
