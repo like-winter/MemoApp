c674b0d9191162ad98aa3a4a0c80ea93c49e9e76
like-winter <jeolmi78@gmail.com>
2026-01-02 04:47:27 +0900

    feat: enable inline editing in memo detail view

--- MemoApp/MemoApp/MemoDetailView.swift
+++ MemoApp/MemoApp/MemoDetailView.swift
@@ -8,20 +8,50 @@
 import SwiftUI
 
 struct MemoDetailView: View {
+    @Environment(\.dismiss) private var dismiss
+    @ObservedObject var viewModel: MemoViewModel
     var memo: Memo
+    
+    @State private var title: String
+    @State private var content: String
+    
+    init(viewModel: MemoViewModel, memo: Memo) {
+        self.viewModel = viewModel
+        self.memo = memo
+        _title = State(initialValue: memo.title)
+        _content = State(initialValue: memo.content)
+        
+    }
     
     var body: some View {
         VStack(spacing: 30) {
             
-            Text(memo.title)
+            TextField("제목", text: $title)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
             
-            Text(memo.content)
+            TextField("Content", text: $content)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
             
             Text(memo.date, style: .date)
+                
+            Button {
+                if viewModel.update(
+                    id: memo.id,
+                    title: title,
+                    content: content)
+                {
+                    dismiss()
+                }
+            } label: {
+                Text("update")
+            }
+            .buttonStyle(.borderedProminent)
         }
     }
 }
 
 #Preview {
-    MemoDetailView(memo: Memo(title: "제목", content: "내용", date: Date()))
+    MemoDetailView(viewModel: MemoViewModel(), memo: Memo(title: "제목", content: "내용", date: Date()))
 }
